343d7f836e2afeae412949a3d7c4f58534431d50
gleidson <angelolustosa@gmail.com>
2023-02-01 21:57:17 -0300

    resolvido

--- scripts.js
+++ scripts.js
@@ -1,28 +1,42 @@
-// let listaDeCompras = ["ovos","leite","manteiga"];
-
-// let listaDeCompras = [];
-// listaDeCompras[0] = "ovos";
-// listaDeCompras[1] = "leite";
-// listaDeCompras[200] = "farinha";
-
-// let listaDeCompras = [];
-// listaDeCompras.push("açucar");
-// listaDeCompras.push("feijão");
-// listaDeCompras.push("arroz");
-
 let tentativas = 0;
 let numeroRandomico = Math.floor(Math.random() * 100) + 1;
+let palpites = [];
+
 
 function checarTentativa(){
     tentativas = tentativas + 1;
 
-    if(tentativas == 5){
+    if(palpites.includes(Number(palpite.value))){
+        alert("Este palpite ja foi dado")
+        return;
+    }else{
+        palpites.push(Number(palpite.value));
+        let respostas = document.getElementById("respostas");
+        respostas.innerText = "";
+        for(let i = 0; i < palpites.length; i++){
+            respostas.innerText += `${palpites[i]} - `;
+        }
+    }
+
+    if(Number(palpite.value) > numeroRandomico){
+        alert("O palpite foi maior do que eu estou pensado");
+    }
+
+    if(Number(palpite.value) < numeroRandomico){
+        alert("O palpite foi menor do que eu estou pensado");
+    }
+
+    if(tentativas < 10){
+        if(Number(palpite.value) == numeroRandomico){
+            alert("Parabens, você acertou o numero: "+ palpite.value);
+            document.getElementById("bia").src = "vitoria.png";
+        }
+    }else{
         alert("Game over");
-        perdeu();
+        document.getElementById("bia").src = "derrota.png";
         return;
     }
 
-    alert(tentativas);
 }
 
 function perdeu(){
